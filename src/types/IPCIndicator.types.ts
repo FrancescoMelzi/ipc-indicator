b3c6a03dcb996ace75f2316656c876b871718282
ivan <ivanvega08@outkook.com>
2022-01-11 16:33:15 -0600

    feat: graph finished

--- src/types/IPCIndicator.types.ts
+++ src/types/IPCIndicator.types.ts
@@ -5,13 +5,3 @@ export interface IPCType {
   price: number
   volume:number
 }
-
-
-/*   
-  x date: "2020-08-18T00:01:43.633-05:00"
-  y price: 39285.85
-  percentageChange: 0.86257
-  volume: 128684937 
-  change: 335.97
-  
-  */
