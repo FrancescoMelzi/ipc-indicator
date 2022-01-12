d884a09af37d8c96be3f117494f956f20c259b46
ivan <ivanvega08@outkook.com>
2022-01-12 12:49:22 -0600

    feat: login finished

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,13 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCyl0G1Ymdyi4Ra_OHFPBxouj_45noW3io",
-  authDomain: "gbmipc.firebaseapp.com",
-  projectId: "gbmipc",
-  storageBucket: "gbmipc.appspot.com",
-  messagingSenderId: "935067650530",
-  appId: "1:935067650530:web:dfde3c69ca0810bd6ed1bb",
-  measurementId: "G-E70PYKB8W5"
+
 }
 
 const initApp = () => {
