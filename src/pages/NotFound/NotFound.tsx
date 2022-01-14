0657bb7f6000e590a2226e29dfb08b1ee19211a9
ivan <ivanvega08@outkook.com>
2022-01-13 18:17:23 -0600

    feat: hosting and not found page

---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './NotFound.css';
+
+const NotFound = () => (
+  <div className='NotFound'>
+    <h1>404 - Not Found!</h1>
+    <Link to="/">Go Home</Link>
+  </div>
+);
+
+export default NotFound;
